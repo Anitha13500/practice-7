5aaac593794d530ab65402c1996afe18dffa3818
Anitha13500 <anithapushpam13500@gmail.com>
2022-10-22 22:37:08 +0530

    add folder

---
+++ operators/main.js
@@ -0,0 +1,88 @@
+//Assignment opperator 
+//Arithmetic Operators
+//Comparison Operators
+//Logical Operators
+//Bitwise operators
+
+    {
+    var a=5;
+    var b=a;
+    console.log(b);
+    }
+
+   { 
+    var a=3;
+     a+=3;
+    console.log(a);
+   }
+   {
+    var a=5;
+    a-=3;
+    console.log(a);
+   }
+   {
+    let num1=7, num2=5;
+    console.log(num1+num2);
+   }
+   {
+    let numa=7,numb=5;
+    sum=(numa-numb);
+    console.log(sum);
+   }
+   {
+   let numa=7,numb=3;
+    sum=(numa**numb);
+    console.log(sum);
+   }
+   {
+    let numa=3;
+    console.log(++numa);
+    console.log(++numa);
+
+   }
+   {
+   let numa=3;
+    console.log(numa++);
+    console.log(numa++);
+   }
+   {
+    let numa=5;
+    console.log(--numa);
+    console.log(--numa);
+   }
+   {
+    let numa=3;
+    console.log(numa--);
+    console.log(numa--);
+   }
+   //Comparison Operators
+   {
+    let numa=5
+    let numb="3";
+    sum=(numa+numb);
+    console.log(sum);
+   }
+   {
+    let numa=5, numb=7;
+    sum=(numa<numb);
+    console.log(sum);
+   }
+   {
+    let numa=5, numb=7;
+    sum=(numa>numb);
+    console.log(sum);
+   }
+  {
+    let numa=5, numb=5;
+    sum=(numa==numb);
+    console.log(sum);
+  }
+  {
+    let numa=5, numb=7;
+    sum=(numa!==numb);
+    console.log(sum);
+  }
+   
+
+
+
